c45166553bbd29fa1c358014e45c2bc21205be10
Saktji <shakthi17902@gmail.com>
2025-10-04 15:00:05 +0530

    Refactor login page into reusable LoginForm component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// export default defineConfig({
+//   base: '/Login-react/', // ← required for GitHub Pages
+//   plugins: [react()],
+// })
+
+
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Login-react/', // ← MUST match your GitHub repo name
   plugins: [react()],
-})
+});
